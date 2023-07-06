43eefc771046debf1377c916aea9cde0eae73853
knagatsuka <knagatsuka@abeam.com>
2023-07-06 16:05:57 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import logo from './logo.svg';
+import './App.css';
 
- 
-
-function App({ signOut }) {
+function App() {
   return (
-<View className="App">
-<Card>
-<Image src={logo} className="App-logo" alt="logo" />
-<Heading level={1}>We now have Auth!</Heading>
-</Card>
-<Button onClick={signOut}>Sign Out</Button>
-</View>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
- 
-
-export default withAuthenticator(App);
+export default App;
 
 
