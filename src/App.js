446738b2216bcb66d1e0b739277df0b57c5f5741
knagatsuka <knagatsuka@abeam.com>
2023-07-10 12:26:34 +0900

    src

--- src/App.js
+++ src/App.js
@@ -10,10 +10,25 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import React, { useEffect, useState } from 'react'
+import {Amplify, API, Auth, Hub } from 'aws-amplify';
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'ap-northeast-1_qrcJMUTdN',
+    userPoolWebClientId: 'nfv9aek2nittvirdff403ahtq',
+    identityPoolId: 'ap-northeast-1:537418d8-ca1e-4718-9b32-fd6f73a36e37',
+    oauth: {
+      domain: 'https://pokemonzya.auth.ap-northeast-1.amazoncognito.com',
+      scope: ['openid'],
+      redirectSignIn: 'https://localhost:3000/',
+      redirectSignOut: 'https://localhost:3000/',
+      responseType: 'code'
+    }
+  }
+})
 
-
-
-function App({ signOut }) {    
+function App({ signOut, user }) {    
   axios
   .get(' https://u3y3rezg17.execute-api.ap-northeast-1.amazonaws.com/Test')
   .then((response) => {
@@ -37,5 +52,6 @@ function App({ signOut }) {
   
 }
 
-export default withAuthenticator(App);
-;
+export default withAuthenticator(App, {
+  signUpAttributes: ['email'],
+})
--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'ap-northeast-1_qrcJMUTdN',
+    userPoolWebClientId: 'nfv9aek2nittvirdff403ahtq',
+    identityPoolId: 'ap-northeast-1:537418d8-ca1e-4718-9b32-fd6f73a36e37',
+    oauth: {
+      domain: 'https://pokemonzya.auth.ap-northeast-1.amazoncognito.com',
+      scope: ['openid'],
+      redirectSignIn: 'https://localhost:3000/',
+      redirectSignOut: 'https://localhost:3000/',
+      responseType: 'code'
+    }
+  }
+})
+
 Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
