c19b75f23e5266002cde405292896a68e387f152
knagatsuka <knagatsuka@abeam.com>
2023-07-10 10:06:01 +0900

    src

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from "./logo.svg";
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
@@ -9,7 +11,18 @@ import {
   Card,
 } from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
+
+
+function App({ signOut }) {    
+  axios
+  .get(' https://u3y3rezg17.execute-api.ap-northeast-1.amazonaws.com/Test')
+  .then((response) => {
+      console.log(response);
+  })
+  .catch((error) => {
+      console.log(error);
+  });
+
   return (
     <View className="App">
       <Card>
@@ -19,6 +32,10 @@ function App({ signOut }) {
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
+
+
+  
 }
 
 export default withAuthenticator(App);
+;
